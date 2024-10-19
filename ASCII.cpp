578fded4264e4a5d37e21340a89fb7331788ae46
Abhigna-31 <abhignasunil206@gmail.com>
2024-10-19 10:27:15 +0530

    Add files via upload

---
+++ ASCII.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	char c;
+	printf("enter a character");
+	scanf("%c",&c);
+	printf("%d display the integer value of a character");
+	printf("%c display the character");
+	printf("ASCII valve of %c= %d",c);
+	return 0;
+}
