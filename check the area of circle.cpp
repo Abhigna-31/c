3de726ab2f8adc49f042d8f6636dae0a3b2fbc37
Abhigna-31 <abhignasunil206@gmail.com>
2024-10-29 09:44:15 +0530

    Add files via upload

---
+++ check the area of circle.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int area,r;
+	printf("enter the radius");
+	scanf("%d%d",&area,&r);
+	{
+		printf("radius of the circle %d\n",r);
+	}
+	{
+		printf("area of the circle %d\n",area);
+	}
+	return 0;
+}
