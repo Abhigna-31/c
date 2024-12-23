d8bbb56a97423740f854b41d7406591e46a0050f
Abhigna-31 <abhignasunil206@gmail.com>
2024-12-23 13:48:25 +0530

    Add files via upload

---
+++ array.1.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+	int i,s,n,a[10];
+	printf("enter the number");
+	scanf("%d",&n);
+	
+	for(i=0;i<n;i++)
+	{
+		printf("a[%d]=",i);
+		scanf("%d",&a[i]);
+	}
+	printf("enter the searching number");
+	scanf("%d",&s);
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==s)
+		{
+			printf("found");
+		    break;
+	    }
+	}
+	if(i==n)
+	{
+		printf("not found");
+	}
+	 
+	
+	return 0;
+}
